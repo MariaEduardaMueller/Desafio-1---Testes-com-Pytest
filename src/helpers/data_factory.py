38218180d4427c2d054801e8689e3875e58b4882
Maria Eduarda Mueller <mariam3@mx2.unisc.br>
2026-06-12 00:35:08 -0300

    Update data_factory.py

--- src/helpers/data_factory.py
+++ src/helpers/data_factory.py
@@ -3,7 +3,7 @@ import uuid
 def gerar_usuario():
     identificador = uuid.uuid4().hex[:8]
     return {
-        "nome": f"Maria Teste {identificador}",
-        "email": f"maria{identificador}@teste.com",
+        "nome": f"NomeAleatorio{identificador}",
+        "email": f"nomealeatorio{identificador}@teste.com",
         "password": "123456",
         "administrador": "true"}
