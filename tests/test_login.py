6893c71a19a25324ae826232e0f5e1f77d3f6515
Maria Eduarda Mueller <mariam3@mx2.unisc.br>
2026-06-14 15:52:30 -0300

    Create test_login.py

---
+++ tests/test_login.py
@@ -0,0 +1,42 @@
+
+
+# Teste de Login Válido
+def test_login_com_sucesso(login_client, usuario_criado):
+    _, payload = usuario_criado
+    response = login_client.login(
+        {
+            "email": payload["email"],
+            "password": payload["password"]
+        })
+    body = response.json()
+    assert response.status_code == 200
+    assert "authorization" in body
+
+# Senha inválida
+def test_login_senha_incorreta(login_client, usuario_criado):
+    _, payload = usuario_criado
+    response = login_client.login(
+        {
+            "email": payload["email"],
+            "password": "senha_errada"
+        })
+    assert response.status_code == 401
+
+# Email Inexistente
+def test_login_email_inexistente(login_client):
+    response = login_client.login(
+        {
+            "email": "naoexiste@email.com",
+            "password": "123456"
+        })
+    assert response.status_code == 401
+
+# Login com campos vázios
+def test_login_campos_vazios(login_client):
+    response = login_client.login(
+        {
+            "email": "",
+            "password": ""
+        })
+    assert response.status_code == 401
+
