01c2de581f87c72f31fada01fc37a082bd7adeab
Maria Eduarda Mueller <mariam3@mx2.unisc.br>
2026-06-14 15:57:10 -0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,12 +7,12 @@ from src.api.usuarios_client import UsuariosClient # Cliente
 from src.api.login_client import LoginClient # Login
 from src.helpers.data_factory import gerar_usuario
 
-#Para login
+#Para os testes de login
 @pytest.fixture
 def login_client():
     return LoginClient()
 
-# Para cliente:
+# Para os testes de usuário:
 @pytest.fixture
 def client():
     return UsuariosClient()
@@ -26,3 +26,14 @@ def usuario_criado(client):
     usuario_id = response.json()["_id"]
     yield usuario_id, payload
     client.excluir_usuario(usuario_id)
+
+# Para os testes de produto
+@pytest.fixture
+def token_admin(login_client, usuario_criado):
+    _, payload = usuario_criado
+    login = login_client.login(
+        {
+            "email": payload["email"],
+            "password": payload["password"]
+        })
+    return login.json()["authorization"]
