56fdda8b516d097a392d9700edef73ad6e43a830
Maria Eduarda Mueller <mariam3@mx2.unisc.br>
2026-06-15 09:34:47 -0300

    Update test_produtos.py
    Adicionei 2 testes novos

--- tests/test_produtos.py
+++ tests/test_produtos.py
@@ -9,12 +9,15 @@ Testes feitos:
 
 4. Cadastro com token
 5. Cadastro sem token
+6. Cadastro de produto com nome duplicado
 
-6. Atualizar produto
+7. Atualizar produto
+8. Atualizar produto inexistente
 
-7. Excluir produto
-8. Excluir teste inexistente
+9. Excluir produto
+10. Excluir produto inexistente
 '''
+
 from src.helpers.data_factory import gerar_produto
 from src.helpers.schemas import validar_schema_produto
 
@@ -45,6 +48,8 @@ def test_cadastrar_produto(produtos_client, token_admin):
     payload = gerar_produto()
     response = produtos_client.cadastrar_produto(payload, token_admin)
     assert response.status_code == 201
+    produto_id = response.json()["_id"]
+    produtos_client.excluir_produto(produto_id, token_admin)
 
 # Cadastro sem token
 def test_cadastrar_produto_sem_token(produtos_client):
@@ -52,15 +57,29 @@ def test_cadastrar_produto_sem_token(produtos_client):
     response = produtos_client.cadastrar_produto(payload, "")
     assert response.status_code == 401
 
-
-# Atualizar produto
-def test_atualizar_produto(produtos_client, token_admin):
+# Cadastro de produto com nome duplicado
+def test_cadastrar_produto_nome_repetido(produtos_client, token_admin):
     payload = gerar_produto()
     cadastro = produtos_client.cadastrar_produto(payload, token_admin)
     produto_id = cadastro.json()["_id"]
+    response = produtos_client.cadastrar_produto(payload, token_admin)
+    assert response.status_code == 400
+    produtos_client.excluir_produto(produto_id, token_admin)
+
+# Atualizar produto
+def test_atualizar_produto(produtos_client, token_admin, produto_criado):
+    produto_id, _ = produto_criado
     novo_payload = gerar_produto()
     response = produtos_client.atualizar_produto(produto_id, novo_payload, token_admin)
     assert response.status_code == 200
+
+# Atualizar produto inexistente
+def test_atualizar_produto_inexistente(produtos_client, token_admin):
+    payload = gerar_produto()
+    response = produtos_client.atualizar_produto("1234567891111111", payload, token_admin)
+    assert response.status_code == 201
+    produto_id = response.json()["_id"]
+    produtos_client.excluir_produto( produto_id, token_admin)
 
 # Excluir produto
 def test_excluir_produto(produtos_client, token_admin):
@@ -70,9 +89,9 @@ def test_excluir_produto(produtos_client, token_admin):
     response = produtos_client.excluir_produto(produto_id, token_admin)
     assert response.status_code == 200
 
-# Excluir teste inexistente
+# Excluir produto inexistente
 def test_excluir_produto_inexistente(produtos_client, token_admin):
-    response = produtos_client.excluir_produto("id_inexistente", token_admin)
-    assert response.status_code == 200
-
-  
+    response = produtos_client.excluir_produto("123456", token_admin)
+    body = response.json()
+    assert response.status_code == 400
+    assert "id" in body
